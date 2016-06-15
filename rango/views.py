0a5105375350c56b537e6b5de38ba121ffd3966f
Igor Shangin <i.n.shangin@yandex.ru>
2016-06-15 14:35:48 +0300

    Добрался до создания формы добавления страниц

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 from rango.models import Category, Page
 
+from rango.forms import CategoryForm
+
 def index(request):
 	context_dict = {'boldmessage': "I am bold font from the context"}
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -31,3 +33,15 @@ def category(request, category_name_slug):
 		pass
 	return render(request, 'rango/category.html', context_dict)
 
+def add_category(request):
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print(form.errors)
+	else:
+		form = CategoryForm()
+	return render(request, 'rango/add_category.html', {'form': form})
