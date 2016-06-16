4665db2b08c91bc39bbd5ed88a080084121f8955
Igor N. Shangin <i.n.shangin@yandex.ru>
2016-06-16 12:21:33 +0300

    need to repair error

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 from rango.models import Category, Page
 
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
 	context_dict = {'boldmessage': "I am bold font from the context"}
@@ -29,6 +29,7 @@ def category(request, category_name_slug):
 
 		context_dict['pages'] = pages
 		context_dict['category'] = category
+
 	except Category.DoesNotExist:
 		pass
 	return render(request, 'rango/category.html', context_dict)
@@ -45,3 +46,27 @@ def add_category(request):
 	else:
 		form = CategoryForm()
 	return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+                cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+        else:
+            print(form.errors)
+    else:
+        form = PageForm()
+
+    context_dict = {'form':form, 'category': cat}
+    return render(request, 'rango/add_page.html', context_dict)
