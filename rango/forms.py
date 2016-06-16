d86a88a7903e6388af6326ebae09c496815ddb07
Igor N. Shangin <i.n.shangin@yandex.ru>
2016-06-16 18:39:46 +0300

    8 section is done

--- rango/forms.py
+++ rango/forms.py
@@ -25,10 +25,7 @@ class PageForm(forms.ModelForm):
 		cleaned_data = self.cleaned_data
 		url = cleaned_data.get('url')
 
-		if url and not url.startwith('http://'):
+		if url and not url.startswith('http://'):
 			url = 'http://' + url
-			cleaned_date['url'] = url
-
+			cleaned_data['url'] = url
 		return cleaned_data
-	
-		
